3eecc4e2e3f7135463d04e183e0c5b3df2bc554d
dwkim <daewoo.kim@microsoft.com>
2016-03-01 19:21:29 +0900

    added CBProcessDAU_DARPUTrigger and CBProcessHAUTrigger base trigger function

--- Functions.cs
+++ Functions.cs
@@ -5,16 +5,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace CloudBread_Scheduler1
 {
+    /// @brief this class is scheuder queue message format
+    public class CBBatchJob
+    {
+        public string JobID { get; set; }
+        public string JobTitle { get; set; }
+        public string JobTriggerDT { get; set; }
+        public string JobTrackID { get; set; }
+    }
+
     public class Functions
     {
         // This function will get triggered/executed when a new message is written 
         // on an Azure Queue called queue.
-        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        public static void CBProcessQueueMessage([QueueTrigger("cloudbread-batch")] CBBatchJob bj, int dequeueCount)
         {
-            log.WriteLine(message);
+            try
+            {
+                Console.WriteLine("CB task Starting {0} at CBProcessQueueMessage", bj.JobID);
+
+                Console.WriteLine("CB task Done {0} at CBProcessQueueMessage", bj.JobID);
+                /// sending notification
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
         }
+
+        /// Timer trigger of CBProcessHAUTrigger
+        public static void CBProcessHAUTrigger([TimerTrigger("0 * */1 * * *")] TimerInfo timer)
+        {
+            try
+            {
+                Console.WriteLine("CB task timer starting at CBProcessHAUTrigger");
+
+                CBBatchJob bj = new CBBatchJob();
+                bj.JobID = "CDBatch-HAU";
+                bj.JobTitle = "Hourly Active User Batch";
+                bj.JobTriggerDT = DateTimeOffset.UtcNow.ToString();
+                bj.JobTrackID = Guid.NewGuid().ToString();
+
+                /// send message to cloudbread-batch queue
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString);
+                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+                CloudQueue queue = queueClient.GetQueueReference("cloudbread-batch");
+                queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(bj)));
+
+                Console.WriteLine("CB task timer done at CBProcessHAUTrigger");
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+
+        /// Timer trigger of CBProcessDAU_DARPUTrigger
+        public static void CBProcessDAU_DARPUTrigger([TimerTrigger("0 5 12 * * *")] TimerInfo timer) // every day 12:05 
+        {
+            try
+            {
+                Console.WriteLine("CB task timer starting at CBProcessDAU_DARPUTrigger");
+
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString);
+
+                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+                CloudQueue queue = queueClient.GetQueueReference("cloudbread-batch");
+
+                /// send message to queue - DAU
+                CBBatchJob bj = new CBBatchJob();
+                bj.JobID = "CDBatch-DAU";
+                bj.JobTitle = "Daily Active User Batch";
+                bj.JobTriggerDT = DateTimeOffset.UtcNow.ToString();
+                bj.JobTrackID = Guid.NewGuid().ToString();
+                queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(bj)));
+
+                /// send message to queue - DARPU
+                bj.JobID = "CDBatch-DARPU";
+                bj.JobTitle = "Daily ARPU Batch";
+                bj.JobTriggerDT = DateTimeOffset.UtcNow.ToString();
+                bj.JobTrackID = Guid.NewGuid().ToString();
+                queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(bj)));
+
+                Console.WriteLine("CB task timer done at CBProcessDAU_DARPUTrigger");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using System.Diagnostics;
 
 namespace CloudBread_Scheduler1
 {
@@ -19,9 +23,31 @@ namespace CloudBread_Scheduler1
         // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
+            /// adding webjob conifg
+            JobHostConfiguration config = new JobHostConfiguration();
+            config.Tracing.ConsoleLevel = TraceLevel.Verbose;
+            
+            /// using timer
+            config.UseTimers();
+
             var host = new JobHost();
             // The following code ensures that the WebJob will be running continuously
             host.RunAndBlock();
+        }
+
+        private static bool VerifyConfiguration()
+        {
+            string webJobsDashboard = ConfigurationManager.ConnectionStrings["AzureWebJobsDashboard"].ConnectionString;
+            string webJobsStorage = ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString;
+
+            bool configOK = true;
+            if (string.IsNullOrWhiteSpace(webJobsDashboard) || string.IsNullOrWhiteSpace(webJobsStorage))
+            {
+                configOK = false;
+                Console.WriteLine("Please add the Azure Storage account credentials in App.config");
+
+            }
+            return configOK;
         }
     }
 }
