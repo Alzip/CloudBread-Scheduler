ce6cc4887c13df7fc0e701e13dd3808ee6d6784e
dwkim <daewoo.kim@microsoft.com>
2016-03-01 21:00:31 +0900

    Revert "Revert "commit for undo""
    This reverts commit c74a1e4ffd613e3ff0b0747726f56091ed0b8914.

--- globalVal.cs
+++ globalVal.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Configuration;
+using System.Configuration; 
 
-namespace CloudBread_Scheduler
+namespace CloudBread_Scheduler.globals
 {
     public static class globalVal
     {
         public static string CBAzureWebJobsDashboard = ConfigurationManager.AppSettings["CBAzureWebJobsDashboard"].ToString();
         public static string CBAzureWebJobsStorage = ConfigurationManager.AppSettings["CBAzureWebJobsStorage"].ToString();
         public static string CBSchedulerDBConnectionString = ConfigurationManager.AppSettings["CBSchedulerDBConnectionString"].ToString();
+        public static int conRetryCount = int.Parse(ConfigurationManager.AppSettings["CloudBreadconRetryCount"]);    /// adding v2.0.0
+        public static int conRetryFromSeconds = int.Parse(ConfigurationManager.AppSettings["CloudBreadconRetryFromSeconds"]);     /// adding v2.0.0
 
         public static string CBNotiEmailSenderID = ConfigurationManager.AppSettings["CBNotiEmailSenderID"];
         public static string CBNotiEmailSenderPassword = ConfigurationManager.AppSettings["CBNotiEmailSenderPassword"];
