240fd6a2a196fd93e0c892e3f1b7bf650c4de648
dwkim <daewoo.kim@microsoft.com>
2016-03-01 20:56:36 +0900

    added notification functions

--- CBNotification.cs
+++ CBNotification.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿/**
+* @brief This Notification functinos are used by batch scheduling service of CloudBread 
+* @author Dae Woo Kim
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Slack.Webhooks;
+using System.Net.Mail;
+using CloudBread_Scheduler.globals;
 namespace CloudBread_Scheduler
 {
-    class CBNotification
+    public static class CBNotification
     {
+        /// @brief this function wrapping send email notication
+        public static string SendEmail(string toAddress, string subject, string body)
+        {
+            string result = "";
+            try
+            {
+                SmtpClient smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com", /// GMail SMTP server
+                    Port = 587,     /// check official gmail site https://support.google.com/mail/answer/13287
+                    /// smtp.gmail.com
+                    /// Use Authentication: Yes
+                    /// Port for TLS/STARTTLS: 587
+                    /// Port for SSL: 465
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new System.Net.NetworkCredential(globalVal.CBNotiEmailSenderID, globalVal.CBNotiEmailSenderPassword),
+                    Timeout = 30000,
+                };
+
+                MailMessage message = new MailMessage(globalVal.CBNotiEmailSenderID, globalVal.CBNotiEmailSenderPassword, subject, body);
+                smtp.Send(message);
+                result = "ok";
+
+                return result;
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+        }
+
+        /// @brief this function wrapping send slack message to channel
+        public static void SendSlackMsg(string Channel, string Text, string Username)
+        {
+            // slack webhook configuration : https://cloudbread.slack.com/apps/manage/A0F7XDUAZ-incoming-webhooks 
+            var slackClient = new SlackClient(globalVal.CBNotiSlackWebhookURL);
+
+            var slackMessage = new SlackMessage
+            {
+                Channel = Channel,
+                Text = Text,
+                Username = Username
+            };
+
+            try
+            {
+                slackClient.Post(slackMessage);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        
     }
 }
--- Functions.cs
+++ Functions.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/**
+* @peojct
+* @brief This functinos are used by batch scheduling service of CloudBread 
+* @author Dae Woo Kim
+*/
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,8 +22,9 @@ using System.Data.Sql;
 using System.Data.SqlClient;
 using Microsoft.Practices.TransientFaultHandling;
 using Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.SqlAzure;
+using CloudBread_Scheduler.globals;
 
-namespace CloudBread_Scheduler1
+namespace CloudBread_Scheduler
 {
     /// @brief this class is scheuder queue message format
     public class CBBatchJob
@@ -30,8 +37,7 @@ namespace CloudBread_Scheduler1
 
     public class Functions
     {
-        // This function will get triggered/executed when a new message is written 
-        // on an Azure Queue called queue.
+        ///
         public static void CBProcessQueueMessage([QueueTrigger("cloudbread-batch")] CBBatchJob bj, int dequeueCount)
         {
             try
@@ -45,7 +51,7 @@ namespace CloudBread_Scheduler1
                 {
                     case "CDBatch-DAU":
                         
-                        using (SqlConnection connection = new SqlConnection(globalVal.DBConnectionString))
+                        using (SqlConnection connection = new SqlConnection(globalVal.CBSchedulerDBConnectionString))
                         {
                             using (SqlCommand command = new SqlCommand("sspBatchDAU", connection))
                             {
@@ -58,7 +64,7 @@ namespace CloudBread_Scheduler1
 
                     case "CDBatch-DARPU":
 
-                        using (SqlConnection connection = new SqlConnection(globalVal.DBConnectionString))
+                        using (SqlConnection connection = new SqlConnection(globalVal.CBSchedulerDBConnectionString))
                         {
                             using (SqlCommand command = new SqlCommand("sspBatchDARPU", connection))
                             {
@@ -71,7 +77,7 @@ namespace CloudBread_Scheduler1
 
                     case "CDBatch-HAU":
 
-                        using (SqlConnection connection = new SqlConnection(globalVal.DBConnectionString))
+                        using (SqlConnection connection = new SqlConnection(globalVal.CBSchedulerDBConnectionString))
                         {
                             using (SqlCommand command = new SqlCommand("sspBatchHAU", connection))
                             {
