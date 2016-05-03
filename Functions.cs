9cd480edbadaa4e67007bb53d1ff6ce72f3d7843
JungHyun Kim <junghyun153@naver.com>
2016-05-03 18:12:28 +0900

    Scheduler: FPC FPU's trigger logic added

--- Functions.cs
+++ Functions.cs
@@ -340,5 +340,46 @@ namespace CloudBread_Scheduler
 
         }
 
+        /// Timer trigger of CBProcessFPC_FPUTrigger
+        public static void CBProcessDAU_DARPUTrigger([TimerTrigger("0 5 12 * * *")] TimerInfo timer) // every day 12:05 
+        {
+            try
+            {
+                Console.WriteLine("CB task timer starting at CBProcessFPC_FPUTrigger");
+
+                // send message to cloudbread-batch queue
+                /// Azure Queue Storage connection retry policy
+                var queueStorageRetryPolicy = new ExponentialRetry(TimeSpan.FromSeconds(2), 10);
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString);
+                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+                queueClient.DefaultRequestOptions.RetryPolicy = queueStorageRetryPolicy;
+                CloudQueue queue = queueClient.GetQueueReference("cloudbread-batch");
+
+                /// send message to queue - FPC
+                CBBatchJob bj = new CBBatchJob();
+                bj.JobID = "CDBatch-FPC";
+                bj.JobTitle = "First Purchase Conversion Batch";
+                bj.JobTriggerDT = DateTimeOffset.UtcNow.ToString();
+                bj.JobTrackID = Guid.NewGuid().ToString();
+                queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(bj)));
+
+                /// send message to queue - FPU
+                bj.JobID = "CDBatch-FPU";
+                bj.JobTitle = "First-time Paying User Batch";
+                bj.JobTriggerDT = DateTimeOffset.UtcNow.ToString();
+                bj.JobTrackID = Guid.NewGuid().ToString();
+                queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(bj)));
+
+                Console.WriteLine("CB task timer done at CBProcessFPC_FPUTrigger");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
     }
 }
